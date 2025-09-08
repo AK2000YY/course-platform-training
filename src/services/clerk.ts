05f8f5fa5cf837cf8014e1301b2684e4ae93cdeb
AK2000YY <abdyabkor@gmail.com>
2025-09-08 19:07:27 +0300

    add function and enable cache

--- src/services/clerk.ts
+++ src/services/clerk.ts
@@ -1,20 +1,23 @@
 import { db } from "@/drizzle/db";
 import { UserRole, UserTable } from "@/drizzle/schema";
-// import { getUserIdTag } from "@/features/users/db/cache";
+import { getUserIdTag } from "@/features/users/db/cache";
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { eq } from "drizzle-orm";
-import { cacheTag } from "next/dist/server/use-cache/cache-tag";
-import { redirect } from "next/navigation";
+import { unstable_cache } from "next/cache";
 
 const client = await clerkClient();
 
-export async function getCurrentUser() {
+export async function getCurrentUser({ allData = false } = {}) {
   const { userId, sessionClaims, redirectToSignIn } = await auth();
 
   return {
     clerkUserId: userId,
     dbId: sessionClaims?.dbId,
     role: sessionClaims?.role,
+    user:
+      allData && sessionClaims?.dbId != null
+        ? await getUser(sessionClaims.dbId)
+        : undefined,
     redirectToSignIn,
   };
 }
@@ -32,12 +35,14 @@ export function syncClerkUserMetadata(user: {
   });
 }
 
-// async function getUser(id: string) {
-//   "use cache";
-//   cacheTag(getUserIdTag(id));
-//   console.log("Called");
-
-//   return db.query.UserTable.findFirst({
-//     where: eq(UserTable.id, id),
-//   });
-// }
+const getUser = (id: string) =>
+  unstable_cache(
+    async () => {
+      console.log("test-id", id);
+      return db.query.UserTable.findFirst({
+        where: eq(UserTable.id, id),
+      });
+    },
+    [id],
+    { tags: [getUserIdTag(id)] }
+  )();
