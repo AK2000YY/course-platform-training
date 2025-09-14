eb4865c92b78174d2d164633c103dce539c175f9
AK2000YY <abdyabkor@gmail.com>
2025-09-14 18:49:16 +0300

    add delete action

--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -4,8 +4,8 @@ import z from "zod";
 import { courseSchema } from "../schemas/coureses";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/services/clerk";
-import { canCreateCourse } from "../permissions/courses";
-import { insertCourse } from "../db/courses";
+import { canCreateCourse, canDeleteCourse } from "../permissions/courses";
+import { insertCourse, deleteCourse as dbDeleteCoures } from "../db/courses";
 
 export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const { success, data } = courseSchema.safeParse(unsafeData);
@@ -20,4 +20,17 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const course = await insertCourse(data);
 
   redirect(`/admin/courses/${course.id}/edit`);
+}
+
+export async function deleteCourse(id: string) {
+  if (!canDeleteCourse(await getCurrentUser()))
+    return {
+      error: true,
+      message: "Error delete your course",
+    };
+  await dbDeleteCoures(id);
+  return {
+    error: false,
+    message: "Successfuly delete your course",
+  };
 }
