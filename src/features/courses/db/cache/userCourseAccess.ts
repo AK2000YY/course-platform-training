e52a786536a8cae24db6ce5c4f62d821e9bf660f
AK2000YY <abdyabkor@gmail.com>
2025-09-15 17:37:38 +0300

    add user course access cache

---
+++ src/features/courses/db/cache/userCourseAccess.ts
@@ -0,0 +1,20 @@
+import { getGlobalTag, getIdTag, getUserTag } from "@/lib/dataCach";
+import { revalidateTag } from "next/cache";
+
+export function getUserCourseAccessGlobalTag() {
+  return getGlobalTag("userCourseAccess");
+}
+
+export function getUserCourseAccessIdTag(courseId: string, userId: string) {
+  return getIdTag("userCourseAccess", `course:${courseId}-user:${userId}`);
+}
+
+export function getUserCourseAccessUserTag(userId: string) {
+  return getUserTag("userCourseAccess", userId);
+}
+
+export function revalidateCourseAccessCache(courseId: string, userId: string) {
+  revalidateTag(getUserCourseAccessGlobalTag());
+  revalidateTag(getUserCourseAccessIdTag(courseId, userId));
+  revalidateTag(getUserCourseAccessUserTag(userId));
+}
