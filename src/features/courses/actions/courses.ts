417b5cf3ec9097603becd1862826472e12f78843
AK2000YY <abdyabkor@gmail.com>
2025-09-15 20:13:37 +0300

    add update action

--- src/features/courses/actions/courses.ts
+++ src/features/courses/actions/courses.ts
@@ -4,8 +4,16 @@ import z from "zod";
 import { courseSchema } from "../schemas/coureses";
 import { redirect } from "next/navigation";
 import { getCurrentUser } from "@/services/clerk";
-import { canCreateCourse, canDeleteCourse } from "../permissions/courses";
-import { insertCourse, deleteCourse as dbDeleteCoures } from "../db/courses";
+import {
+  canCreateCourse,
+  canDeleteCourse,
+  canUpdateCourse,
+} from "../permissions/courses";
+import {
+  insertCourse,
+  deleteCourse as dbDeleteCoures,
+  updateCourse as dbUpdateCourse,
+} from "../db/courses";
 
 export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const { success, data } = courseSchema.safeParse(unsafeData);
@@ -20,6 +28,27 @@ export async function createCourse(unsafeData: z.infer<typeof courseSchema>) {
   const course = await insertCourse(data);
 
   redirect(`/admin/courses/${course.id}/edit`);
+}
+
+export async function updateCourse(
+  id: string,
+  unsafeData: z.infer<typeof courseSchema>
+) {
+  const { success, data } = courseSchema.safeParse(unsafeData);
+
+  if (!success || !canUpdateCourse(await getCurrentUser())) {
+    return {
+      error: true,
+      message: "There is an error updating course",
+    };
+  }
+
+  await dbUpdateCourse(id, data);
+
+  return {
+    error: false,
+    message: "successfuly updating your course",
+  };
 }
 
 export async function deleteCourse(id: string) {
