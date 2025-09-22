028bf043b214d83946328ddc9a58c0a983fe244e
AK2000YY <abdyabkor@gmail.com>
2025-09-22 19:42:25 +0300

    add lesson form and lesson form dialog and sortable lesson list

---
+++ src/features/lessons/comopnents/LessonForm.tsx
@@ -0,0 +1,194 @@
+"use client";
+
+import { LessonStatus, lessonStatuses } from "@/drizzle/schema";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { RequiredLabelIcon } from "@/components/RequiredLabelIcon";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { toast } from "sonner";
+import { lessonSchema } from "../schema/lesson";
+import { Textarea } from "@/components/ui/textarea";
+import { createLesson, updateLesson } from "../action/lesson";
+
+export function LessonForm({
+  sections,
+  onSuccess,
+  lesson,
+  defaultSectionId,
+}: {
+  sections: {
+    id: string;
+    name: string;
+  }[];
+  onSuccess?: () => void;
+  lesson?: {
+    id: string;
+    name: string;
+    status: LessonStatus;
+    youtubeVideoId: string;
+    description: string | null;
+    courseSectionId: string;
+  };
+  defaultSectionId?: string;
+}) {
+  const form = useForm<z.infer<typeof lessonSchema>>({
+    resolver: zodResolver(lessonSchema),
+    defaultValues: lesson ?? {
+      name: "",
+      status: "public",
+      youtubeVideoId: "",
+      description: "",
+      courseSectionId: defaultSectionId ?? sections[0]?.id ?? "",
+    },
+  });
+
+  const videoId = form.watch("youtubeVideoId");
+
+  async function onSubmit(values: z.infer<typeof lessonSchema>) {
+    const action = lesson ? updateLesson.bind(null, lesson.id) : createLesson;
+    const data = await action(values);
+    if (data.error) toast.error("Error", { description: data.message });
+    else {
+      toast.success("Success", { description: data.message });
+      onSuccess?.();
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col gap-6"
+      >
+        <div className="grid grid-cols-1 @lg:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Name
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="youtubeVideoId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>
+                  <RequiredLabelIcon />
+                  Youtube Video Id
+                </FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="courseSectionId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Section</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {sections.map((section) => (
+                      <SelectItem key={section.id} value={section.id}>
+                        {section.name}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="status"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Status</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {lessonStatuses.map((status) => (
+                      <SelectItem key={status} value={status}>
+                        {status}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="min-h-20 resize-none"
+                    {...field}
+                    value={field.value ?? ""}
+                  />
+                </FormControl>{" "}
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="self-end">
+          <Button disabled={form.formState.isSubmitting} type="submit">
+            Save
+          </Button>
+        </div>
+        {/* {videoId && <YoutubeVideoPlayer videoId={videoId} />} */}
+      </form>
+    </Form>
+  );
+}
